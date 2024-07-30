fdba325ed6a272ea3763e43e78792871e838f0e3
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-31 00:18:13 +0100

    design

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -35,8 +35,7 @@ const Navigation = ({setIsHovered,setDisplay}) => {
       <ul className='font-sans text-[42px] pt-4 tracking-tighter cursor-pointer  '>
 
         <motion.li
-        whileHover={{ x: 8 }} 
-     
+        whileHover={{ x: 8 }}
         className='text-[42px] flex align-middle hover:text-[#242323]'
         onMouseEnter={() => handleImageChange([Img])}
         onMouseLeave={() => setIsHovered([Default])}>
