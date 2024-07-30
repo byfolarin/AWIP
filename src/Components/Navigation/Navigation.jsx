c3171c00d9f0a19c6dfbb6e898a3de7e70f8c945
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-30 11:53:17 +0100

    nav-link

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Navigation = () => {
   return (
-    <div className='bg-[#C2BEA9] w-full h-screen max-h-none px-11 py-11'>
-      <ul className='font-sans text-lg'>
-        <li>Home</li>
-        <li>Projects</li>
-        <li>About</li>
-        <li>Culture</li>
+    <div className='bg-[#e1ddc9]  w-3/5 h-screen max-h-none px-11 py-11'>
+      <ul className='font-sans text-[48px] tracking-tighter '>
+        <li >Home</li>
+        <li >Projects</li>
+        <li >About</li>
+        <li >Culture</li>
       </ul>
     </div>
   )
--- src/Components/Navigation/NavigationLeft.jsx
+++ src/Components/Navigation/NavigationLeft.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import img from './Realestate.jpeg'
 
 const NavigationLeft = () => {
   return (
-    <div className='bg-[#a09d8d] w-full h-screen max-h-none px-11 py-11'>
-      
+    <div className='bg-[#a09d8d] w-2/5 h-screen max-h-none'>
+      <div className='w-full bg-[#3c3b34] h-4/5 object-contain'>
+        <img src={img} alt="Real Estate Image" className="object-cover w-full h-full" />
+        </div>
+      <div className='full bg-[#fdfbf0] h-1/5'></div>
     </div>
   )
 }
