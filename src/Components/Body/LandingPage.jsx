8475fc049acb25804c88db60e5cb8025ef636a18
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-31 19:24:03 +0100

    fit

--- src/Components/Body/LandingPage.jsx
+++ src/Components/Body/LandingPage.jsx
@@ -4,10 +4,13 @@ import Img from './image.png'
 const LandingPage = () => {
   return (
     <div className='w-screen h-screen'>
-        <h1 className='text-black text-[248px] tracking-tighter'>AWIP</h1>
 
-        <div className='w-screen bg-[#3c3b34] h-4/5 object-contain'>
-        <img src={Img} alt="" className='w-full h-full object-contain' />
+        <div className='w-fit'>
+        <h1 className='text-black text-[248px] tracking-tighter'>AWIP</h1>
+        </div>
+
+        <div className='bg-[#3c3b34] h-4/5 object-contain'>
+        <img src={Img} alt="" className=' h-full object-contain' />
         </div>
     </div>
   )
