93eec963336a5c0a00901156dae6e0ebf40d30e7
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-31 19:50:29 +0100

    close

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navigation from './Components/Navigation/Navigation'
 import NavigationLeft from './Components/Navigation/NavigationLeft'
 import Default from './Components/Navigation/Default.jpeg'
 import LandingPage from './Components/Body/LandingPage'
+import CloseIcon from './Components/Navigation/CloseIcon'
 
 function App() {
 
@@ -13,14 +14,15 @@ function App() {
   return (
     <div className='bg-[#e1ddc9] '> 
 
+    
+
           <div className='flex justify-end absolute right-0'>
-
-
           {display &&<NavigationLeft isHovered = {isHovered} setIsHovered = {setIsHovered}  />}
           {display && <Navigation setIsHovered = {setIsHovered} />  }
                     
           </div>
-
+          
+          <CloseIcon setDisplay = {setDisplay} />
 
 
           <div className='w-full h-screen overflow-x-clip'>
