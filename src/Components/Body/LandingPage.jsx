10784b79f247e0de63feb4f71bce28f0838237d3
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-31 19:29:37 +0100

    img

--- src/Components/Body/LandingPage.jsx
+++ src/Components/Body/LandingPage.jsx
@@ -9,9 +9,10 @@ const LandingPage = () => {
         <h1 className='text-black text-[248px] tracking-tighter'>AWIP</h1>
         </div>
 
-        <div className='bg-[#3c3b34] h-4/5 object-contain'>
-        <img src={Img} alt="" className=' h-full object-contain' />
+        <div className='bg-[#3c3b34] h-[248px] object-contain'>
+        <img src={Img} alt="" className='w-screen h-full object-contain' />
         </div>
+        
     </div>
   )
 }
