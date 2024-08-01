087ffc5ac236b69ca082c97a895a501e50ab80dc
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-01 16:29:11 +0100

    awip

--- src/Components/Body/LandingPage.jsx
+++ src/Components/Body/LandingPage.jsx
@@ -5,8 +5,14 @@ const LandingPage = () => {
   return (
     <div className='w-screen h-screen'>
 
+      <div className='flex items-center gap-24'>
+
         <div className='w-fit'>
-        <h1 className='text-[#322018] text-[300px] font-thin tracking-[-0.08em]'>AWIP</h1>
+        <h1 className='text-[#322018] text-[300px] font-thin tracking-[-0.08em] leading-tight'>AWIP</h1>
+        </div>
+
+    
+
         </div>
 
         <div className='w-full bg-[#3c3b34] h-[448px]'>
@@ -14,11 +20,6 @@ const LandingPage = () => {
         <img src={Img} alt="" className='object-cover w-full h-full' />
         </div>
 
-        <p>
-        One more glance at the clinic website, elegant and balanced,
-        with neat content arrangement, eye-pleasing color palette, and smooth motion.
-        </p>
-        
     </div>
   )
 }
