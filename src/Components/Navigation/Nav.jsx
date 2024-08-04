00738b694361a98145557fd4dea2aaaf291b08d9
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-04 23:24:58 +0100

    z-index

--- src/Components/Navigation/Nav.jsx
+++ src/Components/Navigation/Nav.jsx
@@ -4,10 +4,8 @@ import Navigation from './Navigation'
 
 const Nav = ({ isHovered, setIsHovered, closeNav }) => {
   return (
-    <div className='flex fixed justify-end w-full bg-[#000000b3] right-0 overflow-hidden overflow-y-hidden'>
-          <NavigationLeft isHovered={isHovered} setIsHovered={setIsHovered} />
-
-          
+    <div className='flex fixed justify-end w-full bg-[#000000b3] z-40 right-0 overflow-hidden overflow-y-hidden'>
+          <NavigationLeft isHovered={isHovered} setIsHovered={setIsHovered} />          
           <Navigation 
         setIsHovered={setIsHovered} 
         closeNav={closeNav}
