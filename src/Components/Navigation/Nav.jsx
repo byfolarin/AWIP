c075be475b403cad90bc92ed9deccd6072ca8f3e
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-05 17:27:54 +0100

    hoverd

--- src/Components/Navigation/Nav.jsx
+++ src/Components/Navigation/Nav.jsx
@@ -4,9 +4,9 @@ import Navigation from './Navigation'
 
 const Nav = ({ isHovered, setIsHovered, closeNav }) => {
   return (
-    <div className='flex fixed justify-end w-full bg-[#000000b3] z-40 right-0 overflow-hidden overflow-y-hidden'>
-          <NavigationLeft isHovered={isHovered} setIsHovered={setIsHovered} />          
-          <Navigation 
+    <div className='flex fixed justify-end w-full bg-[#000000b3] right-0 overflow-hidden overflow-y-hidden'>
+      <NavigationLeft isHovered={isHovered} setIsHovered={setIsHovered} />
+      <Navigation 
         setIsHovered={setIsHovered} 
         closeNav={closeNav}
       />
