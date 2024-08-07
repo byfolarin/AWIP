21ef9f18d3d008c586390ba1060e7f3569d8f36e
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-07 21:08:02 +0100

    iconset

--- src/Components/Navigation/CloseIcon.jsx
+++ src/Components/Navigation/CloseIcon.jsx
@@ -5,9 +5,9 @@ const CloseIcon = ({closeNav}) => {
 
   return (
     <div onClick={closeNav} className='cursor-pointer'>
-     <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M1 1L48.7297 48.7297" stroke="#561D0A" stroke-width="2"/>
-        <path d="M1 48.7297L48.7297 1.00003" stroke="#561D0A" stroke-width="2"/>
+     <svg width="24" height="24" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1 1L48.7297 48.7297" stroke="#561D0A" stroke-width="4"/>
+        <path d="M1 48.7297L48.7297 1.00003" stroke="#561D0A" stroke-width="4"/>
       </svg>  
     </div>
   )
--- src/Components/Navigation/OpenIcon.jsx
+++ src/Components/Navigation/OpenIcon.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
-const OpenIcon = ({ openNav }) => {
+const OpenIcon = ({openNav}) => {
+
   return (
     <div onClick={openNav} className='cursor-pointer'>
-        <svg width="68" height="27" viewBox="0 0 68 27" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0 1.5H67.5" stroke="#561D0A" stroke-width="2"/>
-            <path d="M0 25.5H67.5" stroke="#561D0A" stroke-width="2"/>
+        <svg width="24" height="24" viewBox="0 0 68 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 1.5H67.5" stroke="#561D0A" stroke-width="4"/>
+            <path d="M0 25.5H67.5" stroke="#561D0A" stroke-width="4"/>
         </svg>
     </div>
   )
+
 }
+
+
 
 export default OpenIcon
