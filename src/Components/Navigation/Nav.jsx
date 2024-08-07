29bd4a73355288dc3f106a18662b662169ab975b
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-07 21:45:57 +0100

    shit

--- src/Components/Navigation/Nav.jsx
+++ src/Components/Navigation/Nav.jsx
@@ -1,51 +1,16 @@
 import React from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
 import NavigationLeft from './NavigationLeft'
 import Navigation from './Navigation'
 
-const Nav = ({ isHovered, setIsHovered, closeNav, isOpen }) => {
-  
-  const variants = {
-    hidden: { x: "-100%" },
-    visible: { x: 0 },
-  }
-
+const Nav = ({ isHovered, setIsHovered, closeNav }) => {
   return (
-    <AnimatePresence>
-      {isOpen && (
-        <motion.div 
-          className='flex fixed justify-end w-full z-[999] bg-[#000000b3] right-0 overflow-hidden overflow-y-hidden'
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.3 }}
-        >
-          <motion.div
-            variants={variants}
-            initial="hidden"
-            animate="visible"
-            exit="hidden"
-            transition={{ type: "spring", stiffness: 300, damping: 30 }}
-            className="w-1/3"
-          >
-            <NavigationLeft isHovered={isHovered} setIsHovered={setIsHovered} />
-          </motion.div>
-          <motion.div
-            variants={variants}
-            initial="hidden"
-            animate="visible"
-            exit="hidden"
-            transition={{ type: "spring", stiffness: 300, damping: 30 }}
-            className="w-1/3"
-          >
-            <Navigation 
-              setIsHovered={setIsHovered} 
-              closeNav={closeNav}
-            />
-          </motion.div>
-        </motion.div>
-      )}
-    </AnimatePresence>
+    <div className='flex fixed justify-end w-full z-[50] bg-[#000000b3] right-0 overflow-hidden overflow-y-hidden'>
+      <NavigationLeft isHovered={isHovered} setIsHovered={setIsHovered} />
+      <Navigation 
+        setIsHovered={setIsHovered} 
+        closeNav={closeNav}
+      />
+    </div>
   )
 }
 
