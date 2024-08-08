b5f395c59bb2b968dd9ac06dd3fd759f57c30a1b
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-08 06:37:25 +0100

    stagger

--- src/Components/Navigation/Nav.jsx
+++ src/Components/Navigation/Nav.jsx
@@ -1,16 +1,39 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import NavigationLeft from './NavigationLeft'
 import Navigation from './Navigation'
 
 const Nav = ({ isHovered, setIsHovered, closeNav }) => {
+
+  const variants = {
+    initial: { x: "100%" },
+    animate: { x: 0 },
+    exit: { x: "100%" },
+  }
+
   return (
-    <div className='flex fixed justify-end w-full z-[50] bg-[#000000b3] right-0 overflow-hidden overflow-y-hidden'>
-      <NavigationLeft isHovered={isHovered} setIsHovered={setIsHovered} />
+    <motion.div 
+      className='flex fixed justify-end w-full z-[50] bg-[#000000b3] right-0 overflow-hidden overflow-y-hidden'
+      variants={variants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      transition={{
+        type: "tween",
+        ease: "easeInOut",
+        duration: 0.7
+      }}
+    >
+
+         <NavigationLeft isHovered={isHovered} setIsHovered={setIsHovered} />
+  
+
+
       <Navigation 
         setIsHovered={setIsHovered} 
         closeNav={closeNav}
       />
-    </div>
+    </motion.div>
   )
 }
 
