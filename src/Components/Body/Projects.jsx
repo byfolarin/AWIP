c4da19377c4c747c90bb98b3a929fb0b751a0450
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-09 10:23:28 +0100

    container

--- src/Components/Body/Projects.jsx
+++ src/Components/Body/Projects.jsx
@@ -3,16 +3,34 @@ import React from 'react'
 const Projects = () => {
   return (
     <div>
-      <h1 className='text-[160px] text-[#561D0A] mx-4 font-extralight mt-8 tracking-[-0.11em] font-interTight leading-[1.00]'>Projects</h1>
-      <hr  className='mt-8'/>
+      <h1 className='text-[160px] text-[#561D0A] mx-4 font-extralight my-8 tracking-tighter font-interTight leading-[1.00]'>Projects</h1>
+
+      <div className='mx-8 relative'>
+
+        <div>
+            <div className='flex '>
+                <div className='w-[450px] h-[450px] bg-slate-400 '></div>
+            </div>
+
+            <div className='flex absolute right-0 top-1 '>
+                <div className='w-[250px] h-[250px] bg-slate-500 '></div>
+            </div>
+            </div>
+      </div>
 
 
-      <div className='mx-8'>
 
-      <div className='flex '>
-         <div className='w-[450px] h-[450px] bg-slate-400 '></div>
-      </div> 
 
+      <h1 className='text-[160px] text-[#561D0A] mx-4 font-extralight my-8 tracking-tighter font-interTight leading-[1.00]'>Projects</h1>
+
+      <div className='mx-8 relative'>
+            <div className='flex '>
+                <div className='w-[450px] h-[250px] bg-slate-400 '></div>
+            </div>
+
+            <div className='flex absolute right-0 top-1 '>
+                <div className='w-[450px] h-[150px] bg-slate-500 '></div>
+            </div>
       </div>
     </div>
   )
