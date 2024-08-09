708313ddbc94078bd47fe7248839631999a671a6
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-09 10:53:29 +0100

    img

--- src/Components/Body/Projects.jsx
+++ src/Components/Body/Projects.jsx
@@ -6,12 +6,14 @@ const Projects = () => {
       <h1 className='text-[160px] my-12 text-[#561D0A] mx-4 font-extralight tracking-tighter font-interTight leading-[1.00]'>Projects</h1>
       
 
-        <div className='mx-8'> 
-            <div className='flex items-center relative'>
-                <div className='w-[450px] relative h-[450px] bg-slate-400'></div>
-                <div className='w-[250px] bottom-0 left-0 absolute h-[250px] bg-slate-500'></div>
+      <div className='mx-8'>
+        <div className='flex items-center relative'>
+            <div className='w-[450px] h-[450px] bg-slate-400'></div>
+            <div className='w-[250px] h-[250px] bg-slate-500 absolute bottom-0 left-0 -translate-x-1/2 -translate-y-1/2'>
             </div>
         </div>
+      </div>
+
 
     </div>
   )
