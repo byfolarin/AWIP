d3cf4868f6e3cb465d2804def64796b8714e541a
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-09 12:25:29 +0100

    caps

--- src/Components/Body/Projects.jsx
+++ src/Components/Body/Projects.jsx
@@ -22,9 +22,8 @@ const Projects = () => {
 
   return (
     <div className='my-12 overflow-hidden' ref={containerRef}>
-        
       <h1 className='text-[160px] my-12 text-[#561D0A] mx-4 font-extralight tracking-[-0.11em]
-       font-interTight leading-[1.00]'>Projects</h1>
+       font-interTight leading-[1.00]'>PROJECTS</h1>
       
       <motion.div 
         className='flex gap-[192px] cursor-grab active:cursor-grabbing'
@@ -71,8 +70,23 @@ const Projects = () => {
             <p>Lorem ipsum dolor sit amet consectetur. Mattis egestas ut arcu scelerisque orci mauris. Scelerisque aenean lobortis volutpat in quam. Sit velit libero varius hendrerit egestas aenean.</p>
           </div>
         </div>
+
+
+        <div className='mx-8 flex gap-7'>
+          <div className='flex items-center relative'>
+            <div className='w-[450px] h-[450px] bg-slate-400'></div>
+            <div className='w-[250px] h-[250px] bg-slate-500 absolute bottom-0 left-0 -translate-x-1/2 -translate-y-1/2'>
+            </div>
+          </div>
+
+          <div className='w-[423px]'>
+            <h2 className='text-[#561D0A] text-[64px] font-interTight '>Eagle Consulting</h2>
+            <p>Lorem ipsum dolor sit amet consectetur. Mattis egestas ut arcu scelerisque orci mauris. Scelerisque aenean lobortis volutpat in quam. Sit velit libero varius hendrerit egestas aenean.</p>
+          </div>
+        </div>
       </motion.div>
       
+
     </div>
   )
 }
