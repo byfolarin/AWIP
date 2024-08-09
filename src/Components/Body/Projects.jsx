4a9ac27c7d050b01d9b68e8ed5ff68ff7c23c3ea
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-09 12:59:24 +0100

    gap

--- src/Components/Body/Projects.jsx
+++ src/Components/Body/Projects.jsx
@@ -1,96 +1,43 @@
 import React, { useRef, useEffect, useState } from 'react'
-import { motion, useAnimationControls } from 'framer-motion'
+import { motion } from 'framer-motion'
+import AWIP1 from './AWIP1.jpg'
+import AWIP2 from './AWIP2.jpg'
+import AWIP3 from './AWIP3.jpg'
+import AWIP4 from './AWIP4.jpg'
 
 const Projects = () => {
   const containerRef = useRef(null)
   const [width, setWidth] = useState(0)
-  const controls = useAnimationControls()
-  const autoScrollRef = useRef(null)
-  const scrollSpeedRef = useRef(1)
-  const isDraggingRef = useRef(false)
-  const lastScrollPos = useRef(0)
 
   const projectItems = [
-    { title: "Verraki", description: "Lorem ipsum dolor sit amet consectetur. Mattis egestas ut arcu scelerisque orci mauris. Scelerisque aenean lobortis volutpat in quam. Sit velit libero varius hendrerit egestas aenean." },
-    { title: "Afya Care", description: "Lorem ipsum dolor sit amet consectetur. Mattis egestas ut arcu scelerisque orci mauris. Scelerisque aenean lobortis volutpat in quam. Sit velit libero varius hendrerit egestas aenean." },
-    { title: "Bastion HMO", description: "Lorem ipsum dolor sit amet consectetur. Mattis egestas ut arcu scelerisque orci mauris. Scelerisque aenean lobortis volutpat in quam. Sit velit libero varius hendrerit egestas aenean." },
-    { title: "Eagle Consulting", description: "Lorem ipsum dolor sit amet consectetur. Mattis egestas ut arcu scelerisque orci mauris. Scelerisque aenean lobortis volutpat in quam. Sit velit libero varius hendrerit egestas aenean." },
+    { title: "Verraki", description: "Lorem ipsum dolor sit amet consectetur. Mattis egestas ut arcu scelerisque orci mauris. Scelerisque aenean lobortis volutpat in quam. Sit velit libero varius hendrerit egestas aenean.", image: AWIP1 },
+    { title: "Afya Care", description: "Lorem ipsum dolor sit amet consectetur. Mattis egestas ut arcu scelerisque orci mauris. Scelerisque aenean lobortis volutpat in quam. Sit velit libero varius hendrerit egestas aenean.", image: AWIP2 },
+    { title: "Bastion HMO", description: "Lorem ipsum dolor sit amet consectetur. Mattis egestas ut arcu scelerisque orci mauris. Scelerisque aenean lobortis volutpat in quam. Sit velit libero varius hendrerit egestas aenean.", image: AWIP3 },
+    { title: "Eagle Consulting", description: "Lorem ipsum dolor sit amet consectetur. Mattis egestas ut arcu scelerisque orci mauris. Scelerisque aenean lobortis volutpat in quam. Sit velit libero varius hendrerit egestas aenean.", image: AWIP4 },
   ]
 
   useEffect(() => {
     if (containerRef.current) {
       setWidth(containerRef.current.scrollWidth - containerRef.current.offsetWidth)
     }
-
-    startAutoScroll()
-
-    return () => {
-      if (autoScrollRef.current) {
-        cancelAnimationFrame(autoScrollRef.current)
-      }
-    }
   }, [])
 
-  const startAutoScroll = () => {
-    const scroll = () => {
-      if (!isDraggingRef.current) {
-        lastScrollPos.current -= scrollSpeedRef.current
-        if (lastScrollPos.current <= -width) {
-          lastScrollPos.current = 0
-        } else if (lastScrollPos.current > 0) {
-          lastScrollPos.current = -width
-        }
-        controls.set({ x: lastScrollPos.current })
-      }
-      autoScrollRef.current = requestAnimationFrame(scroll)
-    }
-    autoScrollRef.current = requestAnimationFrame(scroll)
-  }
-
-  const handleMouseMove = (event) => {
-    const { clientX } = event
-    const { left, width } = containerRef.current.getBoundingClientRect()
-    const percentage = (clientX - left) / width
-    scrollSpeedRef.current = 1 + percentage * 2 // Adjust speed based on mouse position
-  }
-
-  const handleMouseLeave = () => {
-    scrollSpeedRef.current = 1 // Reset to default speed
-  }
-
-  const handleDragStart = () => {
-    isDraggingRef.current = true
-  }
-
-  const handleDragEnd = (_, info) => {
-    isDraggingRef.current = false
-    lastScrollPos.current = info.point.x
-  }
-
   return (
-    <div 
-      className='my-12 overflow-hidden' 
-      ref={containerRef}
-      onMouseMove={handleMouseMove}
-      onMouseLeave={handleMouseLeave}
-    >
+    <div className='my-12 overflow-hidden' ref={containerRef}>
       <h1 className='text-[160px] my-12 text-[#561D0A] mx-4 font-extralight tracking-[-0.11em]
        font-interTight leading-[1.00]'>PROJECTS</h1>
       
       <motion.div 
-        className='flex gap-[192px] cursor-grab active:cursor-grabbing'
-        animate={controls}
+        className='flex gap-[64px] cursor-grab active:cursor-grabbing'
         drag="x"
         dragConstraints={{ right: 0, left: -width }}
         dragElastic={0.1}
-        onDragStart={handleDragStart}
-        onDragEnd={handleDragEnd}
       >
         {[...projectItems, ...projectItems, ...projectItems].map((item, index) => (
           <motion.div key={index} className='mx-8 flex gap-7'>
             <div className='flex items-center relative'>
-              <div className='w-[450px] h-[450px] bg-slate-400'></div>
-              <div className='w-[250px] h-[250px] bg-slate-500 absolute bottom-0 left-0 -translate-x-1/2 -translate-y-1/2'>
+              <div className='w-[450px] h-[450px] bg-slate-400'>
+                <img src={item.image} alt={item.title} className='w-full h-full object-cover' />
               </div>
             </div>
 
