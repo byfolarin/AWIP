b567142fce8d3cdffbd47bd543d259ce9f9d7ef0
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-09 19:51:26 +0100

    second-section

--- src/Components/Body/LandingPage.jsx
+++ src/Components/Body/LandingPage.jsx
@@ -6,6 +6,7 @@ import ImageScroll from './ImageScroll';
 import AWIP4 from './AWIP4.jpg'
 import Projects from './Projects';
 import SectionTwo from './SectionTwo';
+import SecondSection from './SecondSection';
 
 
 
@@ -75,16 +76,9 @@ const LandingPage = () => {
 
       <hr  className='mt-8'/>
 
-      
+      <SecondSection />
 
-      <div className='w-full max-w-screen flex justify-between gap-11 my-16 px-8'>
-        {/* <h2 className='text-[48px] 3/5 text-[#00000048] font-interTight font-light tracking-tighter leading-tight'>
-          INTERIOR
-        </h2> */}
-        <p className='w-3/5 text-[#4D3D30] text-[30px] tracking-tight font-interTight font-light'>
-          The AWIP is a modern interpretation of typography found in classic architecture, traditionally etched into stone. This narrative reflected Little Project Co's established history, while maintaining a progressive focus on the future.
-        </p>
-      </div>
+     
 
      <ImageScroll />
 
---
+++ src/Components/Body/SecondSection.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SecondSection = () => {
+  return (
+    <div>
+       <div className='w-full max-w-screen flex justify-between gap-11 my-16 px-8'>
+        {/* <h2 className='text-[48px] 3/5 text-[#00000048] font-interTight font-light tracking-tighter leading-tight'>
+          INTERIOR
+        </h2> */}
+        <p className='w-3/5 text-[#4D3D30] text-[30px] tracking-tight font-interTight font-light'>
+          The AWIP is a modern interpretation of typography found in classic architecture, traditionally etched into stone. This narrative reflected Little Project Co's established history, while maintaining a progressive focus on the future.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default SecondSection
