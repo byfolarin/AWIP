115d86c15cdaa9dafb75f7a30e06613bd631c0b2
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-09 11:12:05 +0100

    steril

--- src/Components/Body/Projects.jsx
+++ src/Components/Body/Projects.jsx
@@ -6,13 +6,18 @@ const Projects = () => {
       <h1 className='text-[160px] my-12 text-[#561D0A] mx-4 font-extralight tracking-tighter font-interTight leading-[1.00]'>Projects</h1>
       
 
-      <div className='mx-8'>
-        <div className='flex items-center relative'>
-            <div className='w-[450px] h-[450px] bg-slate-400'></div>
-            <div className='w-[250px] h-[250px] bg-slate-500 absolute bottom-0 left-0 -translate-x-1/2 -translate-y-1/2'>
+            <div className='mx-8 flex gap-7'>
+                <div className='flex items-center relative'>
+                    <div className='w-[450px] h-[450px] bg-slate-400'></div>
+                    <div className='w-[250px] h-[250px] bg-slate-500 absolute bottom-0 left-0 -translate-x-1/2 -translate-y-1/2'>
+                    </div>
+                </div>
+
+                <div className='w-2/6'>
+                <h2 className='text-[#561D0A] text-[64px] '>Verraki</h2>
+                <p>Lorem ipsum dolor sit amet consectetur. Mattis egestas ut arcu scelerisque orci mauris. Scelerisque aenean lobortis volutpat in quam. Sit velit libero varius hendrerit egestas aenean.</p>
+             </div>
             </div>
-        </div>
-      </div>
 
 
     </div>
