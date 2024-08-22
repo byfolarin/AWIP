d0fec48ba51ecb65cc7f3514c294fb3bebf44fce
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-22 06:41:44 +0100

    marquee

--- src/Components/Body/Clients.jsx
+++ src/Components/Body/Clients.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
+import Marquee from "react-fast-marquee";
 
 const Clients = () => {
   return (
-    <div className='font-interTight flex gap-5 px-6 mt-6 overflow-hidden'>
-      <h1 className='bg-[#561d0a76] rounded-lg text-black text-wrap text-3xl px-6 py-2'>Bastion</h1>
-      <h1 className='bg-[#561d0a76] rounded-lg text-black text-wrap text-3xl px-6 py-2'>Bastion</h1>
+    <Marquee>
+        
+        <div className='font-interTight flex gap-5 px-6 mt-6 overflow-hidden'>
+        <h1 className='bg-[#561d0a76] rounded-lg text-black text-wrap text-3xl px-6 py-2'>Bastion</h1>
+        <h1 className='bg-[#561d0a76] rounded-lg text-black text-wrap text-3xl px-6 py-2'>Bastion</h1>
 
-      <h1 className='bg-[#561d0a76] rounded-lg text-black text-wrap text-3xl px-6 py-2'>Bastion</h1>
-      <h1 className='bg-[#561d0a76] rounded-lg text-black text-wrap text-3xl px-6 py-2'>Bastion</h1>
-      <h1 className='bg-[#561d0a76] rounded-lg text-black text-wrap text-3xl px-6 py-2'>Bastion</h1>
+        <h1 className='bg-[#561d0a76] rounded-lg text-black text-wrap text-3xl px-6 py-2'>Bastion</h1>
+        <h1 className='bg-[#561d0a76] rounded-lg text-black text-wrap text-3xl px-6 py-2'>Bastion</h1>
+        <h1 className='bg-[#561d0a76] rounded-lg text-black text-wrap text-3xl px-6 py-2'>Bastion</h1>
 
-      <h1 className='bg-[#561d0a76] rounded-lg text-black text-wrap text-3xl px-6 py-2'>Bastion</h1>
-      <h1 className='bg-[#561d0a76] rounded-lg text-black text-wrap text-3xl px-6 py-2'>Bastion</h1>
-      <h1 className='bg-[#561d0a76] rounded-lg text-black text-wrap text-3xl px-6 py-2'>Bastion</h1>
+        <h1 className='bg-[#561d0a76] rounded-lg text-black text-wrap text-3xl px-6 py-2'>Bastion</h1>
+        <h1 className='bg-[#561d0a76] rounded-lg text-black text-wrap text-3xl px-6 py-2'>Bastion</h1>
+        <h1 className='bg-[#561d0a76] rounded-lg text-black text-wrap text-3xl px-6 py-2'>Bastion</h1>
 
- 
-    </div>
+    
+        </div>
+    </Marquee>
   )
 }
 
