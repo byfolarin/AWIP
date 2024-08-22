a1f9824767e79b17a5da26f31a1abbbf853b243a
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-22 07:56:54 +0100

    footer

--- src/Components/Body/Footer.jsx
+++ src/Components/Body/Footer.jsx
@@ -4,18 +4,45 @@ const Footer = () => {
   return (
     <div className='bg-[#561D0A] mx-6 rounded-xl mt-9 font-interTight'>
 
-      <p>Menu</p>
 
-      <div className="social-media grid grid-cols-3">
-        <div>Instagram</div>
-        <div>Twitter</div>
-        <div>Facebook</div>
-        <div>LinkedIn</div>
-        <div>Youtube</div>
-        <div>Behance</div>
+      <div>
+      
+
+        <div className="social-media grid grid-cols-3">
+          <div>Instagram</div>
+          <div>Twitter</div>
+          <div>Facebook</div>
+          <div>LinkedIn</div>
+          <div>Youtube</div>
+          <div>Behance</div>
+        </div>
       </div>
+
+
+
+      <div>
+      <p>Menu</p>
+      <div>
+        <div>Home</div>
+        <div>Projects</div>
+        <div>About</div>
+        <div>Contact</div>
+      </div>
+      </div>
+
       
           <h1 className='text-[320px] font-semibold text-[#F7F6E9] flex justify-center tracking-tighter mx-6 leading-snug'>AWIP</h1>
+
+
+          <div className="social-media grid grid-cols-4 border-t py-5 px-10 text-white">
+          <div>Instagram</div>
+          <div>Twitter</div>
+          <div>Facebook</div>
+          <div>LinkedIn</div>
+        </div>
+          
+
+          
     </div>
   )
 }
--- src/Components/Pages/ProjectsSpace.jsx
+++ src/Components/Pages/ProjectsSpace.jsx
@@ -14,7 +14,7 @@ const ProjectsSpace = () => {
 
       <Marquee>
       <h1 className='text-[174px] font-medium my-32 text-[#561D0A]  tracking-[-0.05em] font-interTight leading-[1.00]'>
-       -Projects-Works-Portfolio
+       - Projects - Works - Portfolio
       </h1>
       </Marquee>
 
