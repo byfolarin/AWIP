32429e7ea12b35c20ffcd99594c0de80992b496f
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-22 05:56:48 +0100

    se

--- src/Components/Body/TextRead.jsx
+++ src/Components/Body/TextRead.jsx
@@ -30,75 +30,50 @@ const TextRead = () => {
 
          </div>
 
-      <div className='py-16'>
+           <div className='py-16'>
         
 
+                  <div className='grid grid-cols-3 gap-4 px-4 '>
+
+                              <div className='py-16 px-8 flex flex-col justify-between rounded-xl hover:bg-[#F7F6E9] border border-white bg-[#FFFEF2]'>
+                              <div className='space-y-20'>
+                                    <div>
+                                      <h3 className='text-[32px] tracking-[-0.03em] font-semibold text-[#561D0A] '>Project Completed</h3>
+                                      <p>Over 200 successful projects completed, showcasing our extensive experience and portfolio.</p>
+                                    </div>
+                              </div>
+                              </div>
 
 
 
 
+                              <div className='py-16 px-8 flex flex-col justify-between rounded-xl border hover:bg-[#F7F6E9] border-white bg-[#FFFEF2]'>
+                              <div className='space-y-20'>
+
+                                    <div>
+                                  <h3 className='text-[32px] tracking-[-0.03em] font-semibold text-[#561D0A] '>Interior Design</h3>
+                                  <p>Over 200 successful projects completed, showcasing our extensive experience and portfolio.</p>
+                                </div>
+                          </div>
+                          </div>
 
 
 
+                          <div className='py-16 px-8 flex flex-col justify-between rounded-xl border hover:bg-[#F7F6E9] border-white bg-[#FFFEF2]'>
+                          <div className='space-y-20'>
+                          
+                                <div>
+                                  <h3 className='text-[32px] tracking-[-0.03em] font-semibold text-[#561D0A] '>Space Survey</h3>
+                                  <p>Over 200 successful projects completed, showcasing our extensive experience and portfolio.</p>
+                                </div>
 
-       <div className='grid grid-cols-3 gap-4 px-4 '>
-
-       
-
-
-
-        <div className='py-16 px-8 flex flex-col justify-between rounded-xl hover:bg-[#F7F6E9] border border-white bg-[#FFFEF2]'>
-        <div className='space-y-20'>
-              <div>
-                <h3 className='text-[32px] tracking-[-0.03em] font-semibold text-[#561D0A] '>Project Completed</h3>
-                <p>Over 200 successful projects completed, showcasing our extensive experience and portfolio.</p>
-              </div>
-        </div>
-        </div>
+                                <h2 className='text-[134px] text-[#561D0A] lg:tracking-[-0.05em] font-semibold'>150<span className='text-black'>+</span></h2>
+                          </div>
+                          </div>
 
 
-
-
-        <div className='py-16 px-8 flex flex-col justify-between rounded-xl border hover:bg-[#F7F6E9] border-white bg-[#FFFEF2]'>
-        <div className='space-y-20'>
-
-              <div>
-                <h3 className='text-[32px] tracking-[-0.03em] font-semibold text-[#561D0A] '>Interior Design</h3>
-                <p>Over 200 successful projects completed, showcasing our extensive experience and portfolio.</p>
-              </div>
-        </div>
-        </div>
-
-
-
-        <div className='py-16 px-8 flex flex-col justify-between rounded-xl border hover:bg-[#F7F6E9] border-white bg-[#FFFEF2]'>
-        <div className='space-y-20'>
-        
-              <div>
-                <h3 className='text-[32px] tracking-[-0.03em] font-semibold text-[#561D0A] '>Space Survey</h3>
-                <p>Over 200 successful projects completed, showcasing our extensive experience and portfolio.</p>
-              </div>
-
-              <h2 className='text-[134px] text-[#561D0A] lg:tracking-[-0.05em] font-semibold'>150<span className='text-black'>+</span></h2>
-        </div>
-        </div>
-
-
-        </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                  </div>
+                  
 
       </div>
 
