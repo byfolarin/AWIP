aa209d1cb30bbed22972041ada8201c413fb6d00
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-23 05:08:16 +0100

    flex

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import Marquee from 'react-fast-marquee'
 
 const About = () => {
 
   return (
-    <div>
-        <h1 className='text-[160px] text-[#561D0A] font-light tracking-[-0.11em] font-interTight leading-[1.00]'>
-        ABOUT
+    <div  className='overflow-hidden font-interTight bg-[#FFFEF2] px-6'>
+
+      <Marquee>
+      <h1 className='text-[174px] font-medium my-32 text-[#561D0A]  tracking-[-0.05em] font-interTight leading-[1.00]'>
+       - About us - AWIP 
       </h1>
+      </Marquee>
+
+
+      <section>
+        
+      </section>
+
     </div>
   )
 }
