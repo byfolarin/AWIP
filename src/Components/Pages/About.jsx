37d75487888e0bb40feb5095dc2d94cfaf77f27c
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-23 09:46:13 +0100

    teams

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -5,6 +5,7 @@ import AWIP4 from '../Body/Images/AWIP4.jpg'
 import AWIP2 from '../Body/Images/AWIP2.jpg'
 import AWIP3 from '../Body/Images/AWIP3.jpg'
 import AWIP1 from '../Body/Images/AWIP1.jpg'
+import Teams from '../Body/Teams'
 
 const About = () => {
 
@@ -221,105 +222,10 @@ const About = () => {
 
 
 
-         <div className='w-full max-w-screen flex flex-col items-center lg:flex-end py-12'>
-
-              <h1 className='text-[164px]  text-[#561D0A] font-medium tracking-[-0.04em] font-interTight leading-[1.00]'>
-                Teams
-              </h1>
-
-        </div>
+       <Teams />
 
 
-        <div className='py-16'>
         
-
-                  <div className='grid grid-cols-3 gap-4 px-4 '>
-
-                              <div className='py-16 px-8 flex flex-col justify-between rounded-xl hover:bg-[#F7F6E9] border border-white bg-[#FFFEF2]'>
-                              <div className='space-y-20'>
-                                    <div>
-                                      <h3 className='text-[32px] tracking-[-0.03em] font-semibold text-[#561D0A] '>Project Completed</h3>
-                                      <p>Over 200 successful projects completed, showcasing our extensive experience and portfolio.</p>
-                                    </div>
-                              </div>
-                              </div>
-
-
-
-
-                              <div className='py-16 px-8 flex flex-col justify-between rounded-xl border hover:bg-[#F7F6E9] border-white bg-[#FFFEF2]'>
-                              <div className='space-y-20'>
-
-                                    <div>
-                                  <h3 className='text-[32px] tracking-[-0.03em] font-semibold text-[#561D0A] '>Interior Design</h3>
-                                  <p>Over 200 successful projects completed, showcasing our extensive experience and portfolio.</p>
-                                </div>
-                          </div>
-                          </div>
-
-
-
-                          <div className='py-16 px-8 flex flex-col justify-between rounded-xl border hover:bg-[#F7F6E9] border-white bg-[#FFFEF2]'>
-                          <div className='space-y-20'>
-                          
-                                <div>
-                                  <h3 className='text-[32px] tracking-[-0.03em] font-semibold text-[#561D0A] '>Space Survey</h3>
-                                  <p>Over 200 successful projects completed, showcasing our extensive experience and portfolio.</p>
-                                </div>
-
-                                <h2 className='text-[134px] text-[#561D0A] lg:tracking-[-0.05em] font-semibold'>150<span className='text-black'>+</span></h2>
-                          </div>
-                          </div>
-
-
-                          <div className='py-16 px-8 flex flex-col justify-between rounded-xl border hover:bg-[#F7F6E9] border-white bg-[#FFFEF2]'>
-                          <div className='space-y-20'>
-                          
-                                <div>
-                                  <h3 className='text-[32px] tracking-[-0.03em] font-semibold text-[#561D0A] '>Space Survey</h3>
-                                  <p>Over 200 successful projects completed, showcasing our extensive experience and portfolio.</p>
-                                </div>
-
-                                <h2 className='text-[134px] text-[#561D0A] lg:tracking-[-0.05em] font-semibold'>150<span className='text-black'>+</span></h2>
-                          </div>
-                          </div>
-
-
-
-                          <div className='py-16 px-8 flex flex-col justify-between rounded-xl border hover:bg-[#F7F6E9] border-white bg-[#FFFEF2]'>
-                          <div className='space-y-20'>
-                          
-                                <div>
-                                  <h3 className='text-[32px] tracking-[-0.03em] font-semibold text-[#561D0A] '>Space Survey</h3>
-                                  <p>Over 200 successful projects completed, showcasing our extensive experience and portfolio.</p>
-                                </div>
-
-                                <h2 className='text-[134px] text-[#561D0A] lg:tracking-[-0.05em] font-semibold'>150<span className='text-black'>+</span></h2>
-                          </div>
-                          </div>
-
-
-
-                          <div className='py-16 px-8 flex flex-col justify-between rounded-xl border hover:bg-[#F7F6E9] border-white bg-[#FFFEF2]'>
-                          <div className='space-y-20'>
-                          
-                                <div>
-                                  <h3 className='text-[32px] tracking-[-0.03em] font-semibold text-[#561D0A] '>Space Survey</h3>
-                                  <p>Over 200 successful projects completed, showcasing our extensive experience and portfolio.</p>
-                                </div>
-
-                                <h2 className='text-[134px] text-[#561D0A] lg:tracking-[-0.05em] font-semibold'>150<span className='text-black'>+</span></h2>
-                          </div>
-                          </div>
-
-
-                          
-
-
-                  </div>
-                  
-
-      </div>
 
     </div>
   )
