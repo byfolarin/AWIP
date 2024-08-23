5ea6461bfefbbc0ab5c657b0302419ec8eb6a76e
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-23 11:44:16 +0100

    flexed

--- src/Components/Pages/ProjectDetails/ProjectDetails.jsx
+++ src/Components/Pages/ProjectDetails/ProjectDetails.jsx
@@ -4,7 +4,7 @@ import Vid from './Hvideo.mp4'
 const ProjectDetails = () => {
   return (
     <div>
-      <div className='px-5 bg-[#FFFEF2]'>
+      <div className='px-12 bg-[#FFFEF2] font-interTight leading-snug'>
 
             <div className='flex items-center pt-24 '>
                 <div >
@@ -25,29 +25,45 @@ const ProjectDetails = () => {
             </div>
 
 
-            <div className='flex'>
-                <div>
-                    <h2>
+            <div className='flex gap-8 py-8'>
+
+                <div className='w-[50%]'>
+                    <h2 className='font-semibold text-[#561D0A] text-[32px] pt-5'>
                     The Midnight Elegance Home project involved designing an entire home with a sophisticated dark theme. The focus was on creating a modern, luxurious living space that uses dark colors and rich textures to evoke a sense of elegance and comfort.
                     </h2>
                 </div>
 
-                <div>
+                <div className='w-[40%] font-medium'>
                 <h2>
-                     <span> Project Details </span>
-                        The Coastal Harmony Home project began with an in-depth consultation to understand the client’s vision of a modern coastal retreat. The design concept emphasized natural materials, a soothing color palette, and expansive views of the ocean. Key features included:
+                     <div className='font-semibold text-[24px] text-[#561D0A] py-5'> Project Details </div>
+                        The Coastal Harmony Home project began with an in-depth consultation to understand the client’s vision of a modern coastal retreat. The design concept emphasized natural materials, a soothing color palette, and expansive views of the ocean. Key features included: <br />
 
-                        Open Living Spaces: Creating open-plan living areas to enhance the flow and connection between indoor and outdoor spaces.
+                        <br />
+                       <span className='font-semibold text-[#561D0A]'>Open Living Spaces: </span> Creating open-plan living areas to enhance the flow and connection between indoor and outdoor spaces. <br />
 
-                        Natural Materials: Using reclaimed wood, stone, and natural fibers to evoke a sense of harmony with the surrounding environment.
+                        <br />
+                        <span className='font-semibold text-[#561D0A]'>Natural Materials: </span> Using reclaimed wood, stone, and natural fibers to evoke a sense of harmony with the surrounding environment. <br />
 
-                        Large Windows: Installing floor-to-ceiling windows to maximize natural light and provide unobstructed ocean views.
+                        <br />
+                        <span className='font-semibold text-[#561D0A]'>Large Windows: </span> Installing floor-to-ceiling windows to maximize natural light and provide unobstructed ocean views. <br />
 
-                        Outdoor Living: Designing extensive outdoor areas, including a deck, pool, and garden, for relaxation and entertaining.
+                        <br />
+                        <span className='font-semibold text-[#561D0A]'>Outdoor Living: </span> Designing extensive outdoor areas, including a deck, pool, and garden, for relaxation and entertaining. <br />
 
-                        Modern Amenities: Incorporating state-of-the-art kitchen appliances, smart home technology, and luxurious bathroom fixtures.
+                        <br />
+                        <span className='font-semibold text-[#561D0A]'>Modern Amenities: </span> Incorporating state-of-the-art kitchen appliances, smart home technology, and luxurious bathroom fixtures.
                     </h2>
                 </div>
+            </div>
+
+
+            <div className='rounded-xl '>
+                <div>
+                  <video  className=" w-full object-cover rounded-xl " autoPlay muted loop>
+                    <source src={Vid} type="video/mp4" />
+                    Your browser does not support the video tag.
+                   </video>
+                 </div>
             </div>
 
 
