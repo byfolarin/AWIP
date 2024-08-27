32186d91d499e01144fa95ae435f310b5b010f0d
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-27 05:12:56 +0100

    commit

--- src/Components/Body/Achievements.jsx
+++ src/Components/Body/Achievements.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { motion } from 'framer-motion';
 import { InView } from 'react-intersection-observer';
 
 const CounterAnimation = ({ end, duration }) => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = React.useState(0);
 
   return (
     <motion.span
@@ -22,14 +22,14 @@ const CounterAnimation = ({ end, duration }) => {
   );
 };
 
-const AchievementCard = ({ number, suffix, title, description }) => (
+const AchievementCard = ({ number, suffix, title, description, delay }) => (
   <InView triggerOnce>
     {({ inView, ref }) => (
       <motion.div
         ref={ref}
         initial={{ opacity: 0, y: 50 }}
         animate={inView ? { opacity: 1, y: 0 } : {}}
-        transition={{ duration: 0.5, delay: 0.2 }}
+        transition={{ duration: 0.5, delay }}
         className='py-8 lg:py-16 px-4 lg:px-8 flex flex-col justify-between rounded-xl border border-white bg-[#F7F6E9]'
       >
         <div className='space-y-8 lg:space-y-20'>
@@ -40,7 +40,7 @@ const AchievementCard = ({ number, suffix, title, description }) => (
           <motion.div
             initial={{ opacity: 0 }}
             animate={inView ? { opacity: 1 } : {}}
-            transition={{ duration: 0.5, delay: 0.5 }}
+            transition={{ duration: 0.5, delay: delay + 0.3 }}
           >
             <h3 className='text-2xl lg:text-[32px] tracking-[-0.03em] font-semibold text-[#561D0A]'>{title}</h3>
             <p className='text-sm lg:text-base pt-4 font-interTight'>{description}</p>
@@ -60,18 +60,22 @@ const Achievements = () => {
           suffix="+"
           title="Project Completed"
           description="Over 200 successful projects completed, showcasing our extensive experience and portfolio."
+          delay={0}
         />
         <AchievementCard
           number={13}
           suffix="+"
           title="Years of Experience"
           description="Over a decade of expertise in delivering high-quality solutions to our clients."
+          delay={0.4}
         />
         <AchievementCard
           number={150}
           suffix="+"
           title="Happy Clients"
           description="Over 150 satisfied clients who trust our services and continue to work with us."
+          delay={0.8}
+          
         />
       </div>
     </div>
